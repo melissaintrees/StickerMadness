d52ee9e0c643d9aecdf23e62d3f601657255f3d3
melissaintrees <melissaintrees@gmail.com>
2017-10-16 18:22:22 -0500

    Add data state, data animate and data still attributes as well as if-else loop to give it start gif with click functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,8 +20,8 @@ function createButtons() {
 }
 
 createButtons();
-// When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
 
+// When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
 $("button").on("click", function(event) {
 
   var halloween = $(this).attr("data-name");
@@ -43,14 +43,35 @@ $("button").on("click", function(event) {
       var halloweenImage = $("<img>");
       // add an img attribute of src and pass the collected response to it
       halloweenImage.attr("src", result[i].images.fixed_height_still.url);
+      // add a data attribute called data-still to the img and set it to the gifphy code for a still image:
+      halloweenImage.attr("data-still", result[i].images.fixed_height_still.url);
+      // add a data attribute called data-animate and set it to the giphy code for animated image:
+      halloweenImage.attr("data-animate", result[i].images.fixed_height.url);
+      // add a data attribute called data-state to the img and set it to "still"
+      halloweenImage.attr("data-state", "still");
       // append those new images to the halloween Div variable
       halloweenDiv.append(halloweenImage);
       // put it on the screen
       $("#hallow-gif-image").prepend(halloweenDiv);
       }
+      // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 
-  });
+        // set another on click event for pressing the gifs
+        $("img").on( "click", function(event) {
+          // event.stopPropagation();
+          var state = $(this).attr("data-state");
+          // console.log(state);
+          if (state === "still"){
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+          } else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+        // end on click
+        });
 
+      });
 
 
 // click event function ends
