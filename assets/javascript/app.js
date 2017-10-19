132c81f3314338bbcb089e462357c40585691068
melissaintrees <melissaintrees@gmail.com>
2017-10-19 16:25:12 -0500

    debugging issue with duplicate buttons and add form that takes user input and appends it to the buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,18 @@
-var topics = ["pumpkin", "goblin", "ghost", "candy", "trump", "monsters", "hauntedhouse", "evil"];
-
-
-// Your app should take the topics in this array and create buttons in your HTML.
-// Try using a loop that appends a button for each string in the array.
-function createButtons() {
-  for (var i = 0; i < topics.length; i++ ){
-      console.log(topics[i]);
-      // create a button div
-      var b = $("<button>");
-      // add an class called buttons
-      b.addClass("buttons");
-      // add a data attribute
-      b.attr("data-name", topics[i]);
-      // add the text to the buttons
-      b.text(topics[i]);
-      // Add button to the Html button-holder div
-      $("#button-holder").append(b);
-  }
-}
-
-createButtons();
+var topics = ["pumpkin", "candycorn", "ghost", "apples", "vampires", "monsters", "hauntedhouse", "evil"];
 
 // When the user clicks on a button, the page should grab 10 static, non-animated gif images from the GIPHY API and place them on the page.
-$("button").on("click", function(event) {
 
-  var halloween = $(this).attr("data-name");
-  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + halloween + "&api_key=SIBUreqeKQeIBbZsMZbEinHIO3ZVH8lL&limit=10";
+function generateGifs() {
 
-  $.ajax ({
-    url: queryURL,
-    method: "GET"
-  }).done(function (response) {
+  $("button").on("click", function(event) {
+
+    var halloween = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + halloween + "&api_key=SIBUreqeKQeIBbZsMZbEinHIO3ZVH8lL&limit=10";
+
+    $.ajax ({
+      url: queryURL,
+      method: "GET"
+    }).done(function (response) {
 
     // console.log(queryURL);
     var result = response.data;
@@ -60,30 +42,74 @@ $("button").on("click", function(event) {
       halloweenDiv.append(halloweenRating);
       // put it on the screen
       $("#hallow-gif-image").prepend(halloweenDiv);
+    }
 
+    // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+    // set another on click event for pressing the gifs
+    $("img").on( "click", function(event) {
+      // event.stopPropagation();
+      var state = $(this).attr("data-state");
+      // console.log(state);
+      if (state === "still"){
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
       }
-
-      // When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-
-      // set another on click event for pressing the gifs
-      $("img").on( "click", function(event) {
-        // event.stopPropagation();
-        var state = $(this).attr("data-state");
-        // console.log(state);
-        if (state === "still"){
-          $(this).attr("src", $(this).attr("data-animate"));
-          $(this).attr("data-state", "animate");
-        } else {
-          $(this).attr("src", $(this).attr("data-still"));
-          $(this).attr("data-state", "still");
-        }
-      // end on click
-      });
-
+    // end on animate onclick
     });
 
-    // Under every gif, display its rating (PG, G, so on).
+  // end ajax call
+  });
 
-// click event function ends
+  // end on button click
+  });
+
+};
+
+
+
+// Add a form to your page takes the value from a user input box and adds it into your `topics` array.
+
+$("#add-button").on("click", function(event) {
+  event.preventDefault();
+   // $("#button-holder").hide();
+  // This line grabs the input from the textbox
+  var newButton = $("#halloween-input").val().trim();
+  // Adding input from the textbox to the topics array
+
+  topics.push(newButton);
+  createButtons();
+  generateGifs();
+
 });
 
+// Your app should take the topics in this array and create buttons in your HTML.
+// Try using a loop that appends a button for each string in the array.
+function createButtons() {
+  $("#button-holder").empty();
+
+  for (var i = 0; i < topics.length; i++ ){
+      console.log(topics[i]);
+      // create a button div
+      var b = $("<button>");
+      // add an class called buttons
+      b.addClass("buttons");
+      // add a data attribute
+      b.attr("data-name", topics[i]);
+      // add the text to the buttons
+      b.text(topics[i]);
+      // Add button to the Html button-holder div
+      $("#button-holder").append(b);
+  }
+}
+
+
+// Then make a function call that takes each topic in the array remakes the buttons on the page.
+/// ????
+
+ $(document).on("click", ".buttons", generateGifs);
+  createButtons();
+  generateGifs();
